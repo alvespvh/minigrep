e534762c346dcfe58199e2e38dfe57515f673b94
alves <alvesdevelop@gmail.com>
2025-01-17 21:12:01 -0400

    alterando a funcao new pra build e mundando o tratamento do erro

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // let query = &args[1];
-    // let file_path = &args[2];
 
-    // let config = parse_config(&args);
-
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
 
@@ -27,14 +27,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
 
